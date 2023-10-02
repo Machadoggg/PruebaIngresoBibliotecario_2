58f185d360ebb80ce3ac88703f1d98e91804f7a5
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-10-01 19:28:18 -0500

    Add PrestamoLibro map in PerfilMapeador

--- PruebaIngresoBibliotecario/PruebaIngresoBibliotecario.Api/Utilidad/PerfilMapeador.cs
+++ PruebaIngresoBibliotecario/PruebaIngresoBibliotecario.Api/Utilidad/PerfilMapeador.cs
@@ -1,10 +1,16 @@
 ﻿using AutoMapper;
-
+using PruebaIngresoBibliotecario.Api.Controllers.Prestamos;
+using PruebaIngresoBibliotecario.Dominio.Prestamos;
 
 namespace PruebaIngresoBibliotecario.Api.Utilidad
 {
     public class PerfilMapeador : Profile
     {
-        
+        public PerfilMapeador()
+        {
+            #region
+            CreateMap<PrestamoLibro, PrestamoLibroDTO>().ReverseMap();
+            #endregion
+        }
     }
 }
