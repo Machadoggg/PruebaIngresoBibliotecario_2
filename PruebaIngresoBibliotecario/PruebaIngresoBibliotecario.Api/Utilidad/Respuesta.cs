165798ee1ce228b14b1c3ce68b826b5884cc9711
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-10-01 19:58:38 -0500

    Add properties to Respuesta class

--- PruebaIngresoBibliotecario/PruebaIngresoBibliotecario.Api/Utilidad/Respuesta.cs
+++ PruebaIngresoBibliotecario/PruebaIngresoBibliotecario.Api/Utilidad/Respuesta.cs
@@ -1,6 +1,14 @@
 ﻿namespace PruebaIngresoBibliotecario.Api.Utilidad
 {
-    public class Respuesta
+    public class Respuesta<T>
     {
+        public bool Ok { get; set; } = default!;
+        public T Value { get; set; } = default!;
+        public string MensejeError { get; set; } = default!;
+
+        public Respuesta() 
+        { 
+            Ok = true;
+        }
     }
 }
