ac7d9e81e865a3c972737b1caa741da3e8b18c7d
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-10-01 19:11:06 -0500

    Add properties PrestamoLibroDTO

--- PruebaIngresoBibliotecario/PruebaIngresoBibliotecario.Api/Controllers/Prestamos/PrestamoLibroDTO.cs
+++ PruebaIngresoBibliotecario/PruebaIngresoBibliotecario.Api/Controllers/Prestamos/PrestamoLibroDTO.cs
@@ -4,6 +4,10 @@ namespace PruebaIngresoBibliotecario.Api.Controllers.Prestamos
 {
     public class PrestamoLibroDTO
     {
-        
+        public Guid Id { get; set; }
+        public Guid Isbn { get; set; }
+        public string IdentificacionUsuario { get; set; } = default!;
+        public string TipoUsuario { get; set; } = default!;
+        public DateTime FechaMaximaDevolucion { get; set; }
     }
 }
